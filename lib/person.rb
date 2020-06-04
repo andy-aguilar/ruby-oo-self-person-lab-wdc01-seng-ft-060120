03b3549e2a380eea5debd7230b89c800ee967c78
ianmcknnn <ianmcnnn@gmail.com>
2020-06-04 15:39:58 -0400

    ggot tests to pass and refactored a bit.

--- lib/person.rb
+++ lib/person.rb
@@ -1,42 +1,75 @@
 # your code goes here
 class Person
-    attr_reader :name, :happiness, :hygiene
-    attr_accessor :bank_account
-def initialize (name)
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+  def initialize (name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
-end
+  end
 
-def happiness=(happiness)
-    ## Gotta be a better method for this
-    if happiness <= 10 && happiness >= 0
-        @happiness = happiness
-    elsif happiness > 10
-        @happiness = 10
-    else 
-        @happiness = 0
+  def happiness=(happiness)
+    if happiness > 10
+      @happiness = 10
+    elsif happiness < 0 
+      @happiness = 0
+    else
+      @happiness = happiness
     end
-end
+  end
 
-def hygiene=(hygiene)
-    ## Gotta be a better method for this
-    if hygiene <= 10 && hygiene >= 0
-        @hygiene = hygiene
-    elsif hygiene > 10
-        @hygiene = 10
+  def hygiene=(hygiene)
+    if hygiene > 10
+      @hygiene = 10
+    elsif hygiene < 0 
+      @hygiene = 0
     else 
-        @hygiene = 0
+      @hygiene = hygiene
     end
-end
+  end
 
-def happy?
+  def happy?
     @happiness > 7
-end
+  end
 
-def clean?
+  def clean?
     @hygiene > 7
-end
+  end
 
+  def get_paid(salary)
+    @bank_account += salary
+    'all about the benjamins'
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == 'politics'
+      self.happiness -= 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == 'weather'
+      self.happiness += 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end 
+  end
 end
